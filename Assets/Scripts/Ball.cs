1bb2f313e4d41375b5cf4ea15a328286b360a76e
yevhenkr <vasilenkoevgen88@gmail.com>
2022-07-11 14:38:01 +0300

    Added images bloks.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D col) {
+        // This function is called whenever the ball
+        // collides with something
+    }
+}
